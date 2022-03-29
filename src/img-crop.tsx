be7b2fcd54f37f3609a63dfa5597db844016f821
yoviefp <yoviefp@gmail.com>
2022-03-29 10:38:00 +0700

    change fillColor

--- src/img-crop.tsx
+++ src/img-crop.tsx
@@ -16,7 +16,7 @@ const ImgCrop = forwardRef<Cropper, ImgCropProps>((props, ref) => {
     shape = 'rect',
     grid = false,
     quality = 0.4,
-    fillColor = 'orange',
+    // fillColor = 'orange',
 
     zoom = true,
     rotate = false,
@@ -148,7 +148,7 @@ const ImgCrop = forwardRef<Cropper, ImgCropProps>((props, ref) => {
 
       canvas.width = squareWidth;
       canvas.height = squareHeight;
-      ctx.fillStyle = fillColor;
+      // ctx.fillStyle = fillColor;
       ctx.fillRect(0, 0, squareWidth, squareHeight);
 
       // rotate container
@@ -171,7 +171,7 @@ const ImgCrop = forwardRef<Cropper, ImgCropProps>((props, ref) => {
     } else {
       canvas.width = cropWidth;
       canvas.height = cropHeight;
-      ctx.fillStyle = fillColor;
+      // ctx.fillStyle = fillColor;
       ctx.fillRect(0, 0, cropWidth, cropHeight);
 
       ctx.drawImage(imgSource, cropX, cropY, cropWidth, cropHeight, 0, 0, cropWidth, cropHeight);
@@ -208,7 +208,7 @@ const ImgCrop = forwardRef<Cropper, ImgCropProps>((props, ref) => {
       }
     };
     canvas.toBlob(onBlob, type, quality);
-  }, [fillColor, quality, rotate]);
+  }, [quality, rotate]);
 
   const getComponent = (titleOfModal) => (
     <>
